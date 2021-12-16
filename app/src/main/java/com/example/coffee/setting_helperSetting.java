df13794d1dae42afa7034e290fd92b71c37ae20a
vmd2113 <vmd.2113@gmail.com>
2021-12-16 11:36:08 +0700

    làm phần setting_options_dương

---
+++ app/src/main/java/com/example/coffee/setting_helperSetting.java
@@ -0,0 +1,14 @@
+package com.example.coffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setting_helperSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_helper_setting);
+    }
+}
